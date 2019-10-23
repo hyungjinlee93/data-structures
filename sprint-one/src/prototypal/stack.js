e90279a3395ebd5d1a728ff2de1ab8615174382b
Hyungjin Lee <leehyungjin4u@gmail.com>
2019-10-23 15:24:10 -0700

    Start prototypical stack

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,20 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newStack = Object.create(stackMethods);
+  newStack.storage = {};
 };
 
 var stackMethods = {};
 
+stackMethods.size = function (){
 
+};
+
+stackMethods.pop = function (){
+
+};
+
+stackMethods.push = function (value){
+
+};
