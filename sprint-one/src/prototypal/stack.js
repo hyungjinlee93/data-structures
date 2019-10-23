5ae9b04b7b4f2a8529cfd750060a3176e401d15c
Hyungjin Lee <leehyungjin4u@gmail.com>
2019-10-23 15:27:52 -0700

    Finish prototypical stack

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -3,18 +3,21 @@ var Stack = function() {
   // but try not not reference your old code in writing the new style.
   var newStack = Object.create(stackMethods);
   newStack.storage = {};
+  return newStack;
 };
 
 var stackMethods = {};
 
 stackMethods.size = function (){
-
+  return Object.keys(this.storage).length;
 };
 
 stackMethods.pop = function (){
-
+  let temp = this.storage[Object.keys(this.storage).length - 1];
+  delete this.storage[Object.keys(this.storage).length - 1];
+  return temp;
 };
 
 stackMethods.push = function (value){
-
+  this.storage[Object.keys(this.storage).length] = value;
 };
