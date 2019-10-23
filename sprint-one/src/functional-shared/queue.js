7b2c94d4498b0a4bd334343bafed6c7a6be7187c
Hyungjin Lee <leehyungjin4u@gmail.com>
2019-10-23 15:21:17 -0700

    Finish func-sh queue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -5,12 +5,29 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   someInstance.storage = {};
-  _.extend(someInstance, Queue.queueMethods);
+  _.extend(someInstance, queueMethods);
 
   return someInstance;
 };
 
-Queue.queueMethods = {};
+var queueMethods = {};
 
+queueMethods.size = function (){
+  return Object.keys(this.storage).length;
+}
 
+queueMethods.dequeue = function (){
+  let temp = this.storage['0'];
+  delete this.storage['0'];
+  let counter = 0;
+  for (let key in this.storage){
+    this.storage[counter] = this.storage[key];
+    counter++;
+  }
+  delete this.storage[Object.keys(this.storage).length - 1];
+  return temp;
+}
 
+queueMethods.enqueue = function (value){
+  this.storage[Object.keys(this.storage).length] = value;
+}
