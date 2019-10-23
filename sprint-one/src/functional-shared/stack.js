1d438f7c3efd40c48ba94b34dcf936d97997cb89
Hyungjin Lee <leehyungjin4u@gmail.com>
2019-10-23 12:33:26 -0700

    Start functional instantiation

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,24 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+
+  // Use an object with numeric keys to store values
+  someInstance.storage = {};
+
+  };
 };
 
 var stackMethods = {};
 
+stackMethods.size = function(){
 
+};
+
+stackMethods.pop = function(){
+
+};
+
+stackMethods.push = function(){
+
+};
