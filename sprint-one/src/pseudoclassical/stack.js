99e40681aa01b1fb86c5143ef316d198a0592b33
Hyungjin Lee <leehyungjin4u@gmail.com>
2019-10-23 15:40:39 -0700

    Finish psuedoclassical stack

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,19 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
 };
 
+Stack.prototype.size = function (){
+  return Object.keys(this.storage).length;
+};
 
+Stack.prototype.pop = function (){
+  let temp = this.storage[Object.keys(this.storage).length - 1];
+  delete this.storage[Object.keys(this.storage).length - 1];
+  return temp;
+};
+
+Stack.prototype.push = function (value){
+  this.storage[Object.keys(this.storage).length] = value;
+};
